ab508c22eb70511cbbc3881eac4cb58c800cd0f8
Samara Dias da Silva <marcoscz329@gmail.com>
2026-07-06 16:35:31 -0300

    praticando

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Bateria bateria = new Bateria();
+
+        bateria.setNivel(80);
+        System.out.println("Status: " + bateria.exibirStatus());
+
+
+
+    }
+}
